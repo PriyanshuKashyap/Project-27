555d1bc0643d3177520d99ff07dc8591d5a1020f
PriyanshuKashyap <74148663+PriyanshuKashyap@users.noreply.github.com>
2021-01-24 19:31:28 -0500

    Update Bob.js

--- Bob.js
+++ Bob.js
@@ -1,7 +1,7 @@
 class Bob {
     constructor() {
         var options = {
-            isStatic: false,
+            isStatic: true,
             restitution: 0.3,
             friction: 0.5,
             density: 1.2
@@ -9,20 +9,14 @@ class Bob {
 
         this.radius = 70;
 
-        this.body = Bodies.circle(25, height - 10, this.radius, options);
-        //this.img = loadImage("paper.png");//An error occured here
-        //https://projectsassets.s3.us-east-2.amazonaws.com/dustbingreen.png
-        //https://projectsassets.s3.us-east-2.amazonaws.com/paper.png
+        this.body = Bodies.circle(25, height - 24, this.radius, options);
+        
         World.add(world, this.body);
-        //Task 3 can not be done unless the image is present. Task 3 requires image sizing through investigation, testing, and debugging.
-
+        //fill("rgb(255, 0, 255)");
     }
 
     display() {
         fill("rgb(255, 0, 255)");
-        //push();
-        //ellipse(this.body.position.x, this.body.position.y, 50, 50);
-        //image(this.img, this.body.position.x, this.body.position.y + 27, 50, 50);
-        //pop();
+        ellipse(this.body.position.x, this.body.position.y, 50, 50);
     }
 }
