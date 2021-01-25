849f30cf46cba041df6c0551d9b76b30677613a4
PriyanshuKashyap <74148663+PriyanshuKashyap@users.noreply.github.com>
2021-01-24 19:32:36 -0500

    Update Roof.js

--- Roof.js
+++ Roof.js
@@ -3,13 +3,13 @@ class Roof {
         var options = {
             isStatic: true
         }
-        this.body = Bodies.rectangle(width/2, height - 10, width, 20, options);
+        this.body = Bodies.rectangle(width/2, height - 667, width, 20, options);
 
         World.add(world, this.body);
     }
 
     display() {
-        fill("yellow");
+        fill("grey");
         //push();
         rect(this.body.position.x, this.body.position.y, width, 20);
         //pop();
