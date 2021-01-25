c6c6a84c2064735a4d014033e14ba415d52d4bc7
PriyanshuKashyap <74148663+PriyanshuKashyap@users.noreply.github.com>
2021-01-24 19:33:19 -0500

    Update Rope.js

--- Rope.js
+++ Rope.js
@@ -1,1 +1,36 @@
-//Chain class? Let us discuss this in class.
+class Rope {
+    constructor(body1, body2, offsetX, offsetY){
+        this.offsetX = offsetX;
+        this.offsetY = offsetY;
+        var options = {
+            bodyA: body1,
+            bodyB: body2,
+            pointB: {x: this.offsetX, y: this.offSetY},
+            stiffness: 0.04,
+            length: 10
+        }
+        this.rope = Constraint.create(options);
+        World.add(world, this.rope);
+    }
+
+    fly() {
+        //this.sling.bodyA = null;
+    }
+
+    display(){
+        //if (this.rope.bodyA) {
+            var pointA = this.rope.bodyA.position;
+            var pointB = this.rope.bodyB.position;
+            
+            
+        //}
+        /*var pointA1 = this.rope.bodyA.position.x;
+        var pointA2 = this.rope.bodyA.position.y;
+        var pointB1 = this.rope.bodyB.position.x;
+        var pointB2 = this.rope.bodyB.position.y;*/
+        strokeWeight(4);
+        line(pointA.x, pointA.y, this.offsetX, pointB.y);
+        
+    }
+    
+}
